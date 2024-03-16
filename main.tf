c8971d1834762af89aba8d3978c72d4f84addc03
Scott Bly <76767092+scottblydotcom@users.noreply.github.com>
2024-03-16 15:25:07 -0700

    alb module version fix

--- main.tf
+++ main.tf
@@ -48,6 +48,7 @@ resource "aws_instance" "blog" {
 
 module "alb" {
   source              = "terraform-aws-modules/alb/aws"
+  version             = "~> 6.0"
 
   name                = "blog-alb"
 
